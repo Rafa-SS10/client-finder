10e3342910e0e8476983879d3a301b4742103e2f
Rafael Santos <rafa-silvasantos@hotmail.com>
2025-10-29 23:03:14 +0000

    fix for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    base: '/',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
 });
 
